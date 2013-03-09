d18543e8f15a57568291a906073ac0ae83455afa
Jaap Groeneveld <jgroeneveld@me.com>
2013-03-09 16:22:07 +0100

    grouping cov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,5 +5,10 @@ if ENV["COVERAGE"]
     add_filter '/config/'
     add_filter '/vendor/'
     add_filter '/lib/tasks/'
+
+    add_group 'Core', 'lib/core'
+    add_group 'CRM', 'lib/crm'
+    add_group 'Billing', 'lib/billing'
+    add_group 'Ticket System', 'lib/ticket_system'
   end
 end
