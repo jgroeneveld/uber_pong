5d7369eedd545b8c7ea388208f91e3064349716a
Jaap Groeneveld <jgroeneveld@me.com>
2013-03-13 09:06:31 +0100

    kommentare raus

--- lib/core/entity.rb
+++ lib/core/entity.rb
@@ -5,38 +5,29 @@ module Core
     class UndefinedSetter < Exception; end
     attr_accessor :attributes
 
-    # module ClassMethods
-      def self.property(name, klass)
-        define_method "#{name}=" do |value|
-          self.attributes[name.to_sym] = value
-          instance_variable_set("@#{name}", value)
-        end
-
-        define_method "#{name}" do
-          instance_variable_get("@#{name}")
-        end
+    def self.property(name, klass)
+      define_method "#{name}=" do |value|
+        self.attributes[name.to_sym] = value
+        instance_variable_set("@#{name}", value)
       end
-    # end
 
-    # module InstanceMethods
-      def initialize(args={})
-        self.attributes = {}
-
-        args.each { |k,v|
-          if self.respond_to?("#{k}=")
-            self.attributes[k] = v
-            self.send("#{k}=", v)
-          else
-            raise UndefinedSetter
-          end
-        }
+      define_method "#{name}" do
+        instance_variable_get("@#{name}")
       end
-    # end
+    end
 
-    # def self.included(receiver)
-    #   receiver.extend         ClassMethods
-    #   receiver.send :include, InstanceMethods
-    # end
+    def initialize(args={})
+      self.attributes = {}
+
+      args.each { |k,v|
+        if self.respond_to?("#{k}=")
+          self.attributes[k] = v
+          self.send("#{k}=", v)
+        else
+          raise UndefinedSetter
+        end
+      }
+    end
 
   end
 end
